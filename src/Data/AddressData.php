2ddfb889a3acb4111e6f157aef95df9f74a5d004
hamzah <hamzahnafalah@gmail.com>
2025-09-29 12:45:48 +0700

    perbaikan pendaftaran pasien dan pemeriksaan

--- src/Data/AddressData.php
+++ src/Data/AddressData.php
@@ -45,7 +45,25 @@ class AddressData extends Data implements DataAddressData{
     #[MapInputName('village_id')]
     public ?int $village_id = null;
 
+    #[MapName('village_model')]
+    #[MapInputName('village_model')]
+    public ?object $village_model = null;
+
     #[MapName('props')]
     #[MapInputName('props')]
     public ?AddressPropsData $props = null;
+
+    public static function after(self $data): self{
+        $new = self::new();
+        $props = &$data->props->props;
+
+        if (isset($data->village_id)){
+            $data->village_model = $village_model = $new->VillageModel()->findOrFail($data->village_id);
+            $data->province_id ??= $village_model->province_id;
+            $data->district_id ??= $village_model->district_id;
+            $data->subdistrict_id ??= $village_model->subdistrict_id;
+            $props['prop_village'] = $village_model->toViewApi()->resolve();
+        }
+        return $data;
+    }
 }
